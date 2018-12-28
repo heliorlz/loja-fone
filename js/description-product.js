49c7fa4d9c80c4ccef98c601124d2976ab70fbbe
Helio Alves <heleralvess@gmail.com>
2018-12-28 07:42:34 -0200

    desafio 01 - pegar o elemento e mostra-lo no console

--- js/description-product.js
+++ js/description-product.js
@@ -5,3 +5,7 @@ $heart.addEventListener('click', handleClick);
 function handleClick() {
     console.log('Ae');
 };
+
+const $addCart = window.document.querySelector('.-second');
+
+console.log($addCart);
