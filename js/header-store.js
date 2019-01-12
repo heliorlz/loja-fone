4d748dbde571db5acdca78cf68fb7fcee1a8d754
Helio Alves <heleralvess@gmail.com>
2019-01-12 13:26:07 -0200

    desafio unico - implementar a feature menu hamburguer

--- js/header-store.js
+++ js/header-store.js
@@ -1,19 +1,32 @@
-const $magnifying = document.querySelector('.magnifying');
 const $search = document.querySelector('.wrap-search');
-const $close = document.querySelector('.close');
+const $navbar = document.querySelector('.navbar');
+const $navigation = document.querySelector('.navigation')
 
-$magnifying.addEventListener('click', function () { 
-    $search.classList.toggle('-active');
+const $magnifying = $search.querySelector('.magnifying');
+const $close = $search.querySelector('.close');
+
+const $toggle = $navigation.querySelector('.toggle');
+
+$magnifying.addEventListener('click', (event) => { 
+    event.preventDefault();
+    $search.classList.add('-active');
     if ($search.classList.contains('-active')) {
         $magnifying.classList.add('-hidden');
         $close.classList.remove('-hidden');
     }
 })
 
-$close.addEventListener('click', function () {
-    $search.classList.toggle('-active');
+$close.addEventListener('click', (event) => {
+    event.preventDefault(); // para impedir que o input:image tente enviar um formulário, sua caracteristica padrão
+    $search.classList.remove('-active');
     $magnifying.classList.remove('-hidden');
     $close.classList.add('-hidden');
+})
+
+$toggle.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log('peguei');
+    $navbar.classList.toggle('-hidden');
 })
 
 
